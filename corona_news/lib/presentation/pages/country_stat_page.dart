0ed183febf59d9132dbde3cb69a91e6b0ce37561
Kenan Yusubov <kanan.yusubov.1999@gmail.com>
2020-03-26 00:47:31 +0400

    search option is added

--- corona_news/lib/presentation/pages/country_stat_page.dart
+++ corona_news/lib/presentation/pages/country_stat_page.dart
@@ -1,4 +1,5 @@
 import 'package:corona_news/blocs/covid_bloc/covid_bloc.dart';
+import 'package:corona_news/data/models/country.dart';
 import 'package:corona_news/presentation/widgets/country_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,6 +18,17 @@ class CountryStatPage extends StatelessWidget {
           'Ölkələr',
           style: TextStyle(color: Colors.white),
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: CountrySearch(),
+              );
+            },
+          ),
+        ],
       ),
       body: SafeArea(
         child: Column(
@@ -34,6 +46,60 @@ class CountryStatPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class CountrySearch extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // actions for app bar
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // leading icon on the left of the app bar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // show some result based on selection
+    return CountryItem(Country(
+      name: 'Test',
+      newCases: '0',
+      newDeaths: '0',
+      totalCases: '0',
+      totalDeaths: '0',
+      totalRecovered: '0',
+    ));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // show when someone searches for something
+    return ListView.builder(
+      itemBuilder: (context, i) => ListTile(
+        onTap: () => showResults(context),
+        title: Text('$i test'),
+      ),
+      itemCount: 5,
     );
   }
 }
--- corona_news/lib/presentation/widgets/custom_expansion_tile.dart
+++ corona_news/lib/presentation/widgets/custom_expansion_tile.dart
@@ -15,12 +15,22 @@ class CustomExpansionTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
-      title: Text(utf8.decode(title.runes.toList())),
+      title: Text(
+        utf8.decode(title.runes.toList()),
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Html(
-            data: utf8.decode(body.runes.toList()),
+            defaultTextStyle: TextStyle(
+              fontSize: 18,
+            ),
+            data: utf8.decode(
+              body.runes.toList(),
+            ),
           ),
         )
       ],
